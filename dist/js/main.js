7f33ac42d08b5fe3dfb9db0fb15341cd5947c38b
SvetlanaRyzhova98 <sweta300498@yandex.ru>
2021-12-05 23:06:53 +0300

    dist

--- dist/js/main.js
+++ dist/js/main.js
@@ -24,6 +24,7 @@ $(".menu-burger").on("click", function () {
   $(".menu-burger").toggleClass("open-menu");
   $(".menu-mobile").toggleClass("open-menu");
 });
+/*--------------*/
 
 /*slider and pagination start*/
 const slider = document.querySelector(".slider");
@@ -65,6 +66,7 @@ $(function () {
     infinite: true,
     focusOnSelect: true,
     variableWidth: true,
+    centerMode: true,
     dots: true,
     dotsClass: "slick-dots",
     slidesToShow: 2,
@@ -89,6 +91,7 @@ $(function () {
     slidesToShow: 3,
     slidesToScroll: 1,
     variableWidth: true,
+    centerMode: true,
 
     nextArrow:
       '<img class="slider-arrows-slick  slider-arrows__left-slick" src="img/icon/arrow-left.png" alt="">',
@@ -119,15 +122,7 @@ $(function () {
       },
     ],
   });
-  // $(".slick").slick({
-  //   slidesToShow: 4,
-  //   slidesToScroll: 1,
-  //   infinite: true,
-  //   prevArrow:
-  //     '<img class="slider-arrows  slider-arrows__left" src="img/arrow-left.png" alt="">',
-  //   nextArrow:
-  //     '<img class="slider-arrows  slider-arrows__right" src="img/arrow-right.png" alt="">',
-  // });
+
   $(".slick-mobile").slick({
     prevArrow:
       '<img class="slider-arrows  slider-arrows__right" src="img/icon/arrow-right.png" alt="">',
@@ -139,6 +134,7 @@ $(function () {
     slidesToScroll: 1,
     slidesToShow: 2,
     variableWidth: true,
+    centerMode: true,
     mobileFirst: true,
     responsive: [
       {
@@ -147,4 +143,47 @@ $(function () {
       },
     ],
   });
+  $(".popular__cards").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    infinite: true,
+    variableWidth: true,
+    nextArrow:
+      '<img class="slider-arrows-slick  slider-arrows__left-slick" src="img/icon/arrow-left.png" alt="">',
+    prevArrow:
+      '<img class="slider-arrows-slick  slider-arrows__right-slick" src="img/icon/arrow-right.png" alt="">',
+  });
+});
+/*---------------------search--start------------------------*/
+$(function () {
+  $(".search-btn__fake-submit").on("click", function (event) {
+    event.preventDefault();
+    $(".search-btn").addClass("_search-focus");
+    $(".search-close").addClass("search-close_visible");
+  });
+  $(document).on("click", function (event) {
+    let searchBlock = ".search-btn";
+    let searchClose = ".search-close";
+    if ($(event.target).closest(searchBlock).length) return;
+    $(searchBlock).removeClass("_search-focus");
+    $(searchClose).removeClass("search-close_visible");
+    event.stopPropagation();
+  });
+  $(".search-close").on("click", function (event) {
+    let searchBlock = ".search-btn";
+    let searchClose = ".search-close";
+    $(searchBlock).removeClass("_search-focus");
+    $(searchClose).removeClass("search-close_visible");
+    event.stopPropagation();
+  });
+});
+/*------------------scroll top----------*/
+$(".footer-mobile__link-item_up").on("click", function () {
+  $([document.documentElement, document.body]).animate(
+    {
+      scrollTop: $(".header").offset().top,
+    },
+    2000
+  );
 });
